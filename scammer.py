6e8e1e42d2a21896b4d77ae5093d95005bb54259
akaplan521 <alexa.kaplan@uvm.edu>
2024-04-13 16:14:15 -0400

    csv ml and minor

--- scammer.py
+++ scammer.py
@@ -1,18 +1,59 @@
 from flask import Flask, render_template, request, jsonify, url_for, redirect
 import traceback
-import pandas, nltk
 
+import string
+import pandas as pd
+import numpy as np 
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+nltk.download('stopwords')
+
+df = pd.read_csv('spam_ham_dataset.csv')
+df['text'] = df['text'].apply(lambda x: x.replace('\r\n', ' '))
+    
+stemmer = PorterStemmer()
+corpus = []
+
+stopwords_set = set(stopwords.words('english'))
+
+for i in range(len(df)):
+    text = df['text'].iloc[i].lower()
+    splitText = text.translate(str.maketrans('','', string.punctuation)).split()
+    stemList = []
+    for word in splitText:
+        stem = stemmer.stem(word)
+        if stem not in stopwords_set:
+            stemList.append(stem)
+
+    corpus.append(' '.join(stemList))
+    
+
+vectorizer = CountVectorizer()
+x = vectorizer.fit_transform(corpus).toarray()
+y = df.label_num
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
+clf = RandomForestClassifier(n_jobs=-1)
+clf.fit(x_train, y_train)
+
+def detectScam(inputText):
+    text = inputText.lower().translate(str.maketrans('', '', string.punctuation)).split()
+    text = [stemmer.stem(word) for word in text if word not in stopwords_set]
+    text = ' '.join(text)
+
+    email = [text]
+    x_email = vectorizer.transform(email)
+    return clf.predict(x_email)
 
 app = Flask(__name__, static_folder='static')
-
-def detectScam(text):
-     countFlags = 0
-     badwords= ['vulnerability', 'click here', 'click here to', 'security reasons', 'urgent', 'postal', 'alert', 'immediate']
-     
-     for word in badwords:
-        if text.contains(word):
-            countFlags = countFlags + 1
-
 
 @app.route('/')
 def start():
@@ -34,15 +75,18 @@ def test():
 
 @app.route('/reemail', methods=['POST'])
 def reemail():
-        print('hello')
+        
         data = request.get_json()
         text = data.get('text')
         print(text)
         scam = False
-        flags = ['']
+        phish = detectScam(text)
+        if phish == 1:
+            phish = 'Phishing Detected.'
+        else:
+            phish = 'This message appears to be safe.'
 
-        print(text)
+        print(phish)
             
-        return render_template('test.html', flags = flags, scam = scam)
+        return render_template('test.html', phish = phish)
         
-
