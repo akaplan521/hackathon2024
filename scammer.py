d03240e00678c06074a3285b9352e251148e1e75
akaplan521 <alexa.kaplan@uvm.edu>
2024-04-13 10:02:23 -0400

    set up

--- scammer.py
+++ scammer.py
@@ -2,12 +2,18 @@ from flask import Flask, render_template, request, jsonify, url_for, redirect
 import traceback
 
 
-# Defines flask app and specifys where to look for html and static resources
-app = Flask(__name__, template_folder='/', static_folder='/static')
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def start():
-    return 0
+    return render_template('index.html')
 
+#@app.route('/info')
+#def info():
+#    return 0
+
+#@app.route('/test')
+#def test():
+#    return 0
 
 
